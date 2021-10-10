fbe78ecfcd526d7907c31e1f02f2b61ffb662162
atee001 <atee001@ucr.edu>
2021-10-09 20:26:40 -0700

    Ex_1_main

--- source/main.c
+++ source/main.c
@@ -1,7 +1,7 @@
 /*	Author: atee001
  *  Partner(s) Name: 
  *	Lab Section:
- *	Assignment: Lab #  Exercise #
+ *	Assignment: Lab # 3  Exercise # 1
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
@@ -13,11 +13,31 @@
 #endif
 
 int main(void) {
-    /* Insert DDR and PORT initializations */
 
-    /* Insert your solution below */
+    DDRA = 0x00; PORTA = 0xFF;
+    DDRB = 0x00; PORTB = 0xFF;
+    DDRC = 0xFF; PORTC = 0x00;
+
+    unsigned char countA = 0x00;
+    unsigned char countB = 0x00;
+    unsigned char temp = 0x00;
+
     while (1) {
+	
+	temp = PINA;
+	while(temp%2){
+		countA++;
+		temp /= 2;
+	}
 
+	temp = PINB;
+	while(temp%2){
+		countB++;
+		temp/=2;
+	}
+	
+	PORTC = countA + countB;
+	
     }
     return 1;
 }
